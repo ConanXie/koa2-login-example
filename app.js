70303cc68c7dc813f9de1b3ae36df9bc4d08778c
ConanXie <xjj416@gmail.com>
2016-09-02 18:21:29 +0800

    add config

--- app.js
+++ app.js
@@ -7,23 +7,32 @@ import session from 'koa-generic-session'
 import redisStore from 'koa-redis'
 import convert from 'koa-convert'
 
+// config
+import { port } from './config'
+
 const app = new Koa()
 const router = koaRouter()
+
 // session
 app.keys = ['my-secret']
 // redis session
 app.use(convert(session({
   store: redisStore()
 })))
+
 // bodyparser
 app.use(bodyparser())
+
 // views
 app.use(views(__dirname + '/views', {
   map: {
     html: 'nunjucks'
   }
 }))
+
 // routes
+app.use(router.routes())
+   .use(router.allowedMethods())
 import routes from './routes'
 routes(app)
 
@@ -33,6 +42,6 @@ app.use(serve('static'))
 // mongodb
 import './model/config'
 
-app.listen(3010, () => {
-  console.log(`Listening on 3010`)
+app.listen(port, () => {
+  console.log(`Listening on ${port}`)
 })
