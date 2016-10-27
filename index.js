027358fe2467cd698ba266dba6c5d9e35f43424f
ConanXie <xjj416@gmail.com>
2016-10-27 20:40:41 +0800

    use node v7.0.0 async/await

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
-require('babel-polyfill')
+// require('babel-polyfill')
 require('babel-core/register')({
-  presets: ['es2015', 'stage-0']
+  // presets: ['es2015', 'stage-0']
+  plugins: ["transform-es2015-modules-commonjs"]
 })
 require('./app')
