0d5ad9305db628e8368e0096c85dd1d214afe97b
ConanXie <xjj416@gmail.com>
2017-09-02 23:38:28 +0800

    Remove strict mode

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-'use strict'
-
-// require('babel-polyfill')
 require('babel-core/register')({
-  // presets: ['es2015', 'stage-0']
-  plugins: ["transform-es2015-modules-commonjs"]
+  plugins: ['transform-es2015-modules-commonjs']
 })
+
 require('./app')
